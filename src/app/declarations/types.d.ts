0da0fff0bd97ebf04c50ac3b41cc4d6bdc09a92b
Mittal Shah <mittal.shah@timeclockplus.com>
2021-04-12 14:19:06 -0500

    V7-19178 #comment Added more inputs

--- src/app/declarations/types.d.ts
+++ src/app/declarations/types.d.ts
@@ -1,2 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
+import {LogOnConfig, LogOnData} from './global';
+
 type AnyType = any;
+type LogOnContext = {
+  data: LogOnData;
+  config: LogOnConfig
+};
