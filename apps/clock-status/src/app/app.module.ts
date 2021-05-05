cb0bc87deafe0606c9036842d386d8d7ce742977
Mittal Shah <mittal.shah@timeclockplus.com>
2021-05-05 14:43:05 -0500

    V7-19178 #comment fixed duplicate component declaration error. consolidated modules.

--- apps/clock-status/src/app/app.module.ts
+++ apps/clock-status/src/app/app.module.ts
@@ -1,61 +1,13 @@
 import { NgModule } from '@angular/core';
-
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { MatInputModule } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatButtonModule } from '@angular/material/button';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { LogOnComponent } from './features/log-on/log-on.component';
-
-import {
-  AlertDialogComponent,
-  DEFAULT_TIMEOUT,
-  DropdownInputComponent,
-  EditableInputComponent,
-  LoadingHttpInterceptor,
-  RequestLoadingComponent,
-  RequestLoadingService,
-  TimeoutHttpInterceptor,
-} from '@tcp/tcp-ng-ui';
 import { AppRoutingModule } from './app-routing.module';
+import { TcpNgUiModule } from '../../../../libs/tcp-ng-ui/src/tcp-ng-ui.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LogOnComponent,
-    AlertDialogComponent,
-    EditableInputComponent,
-    DropdownInputComponent,
-    RequestLoadingComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    FormsModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatInputModule,
-    MatButtonModule,
-    MatDividerModule,
-    MatDialogModule,
-    MatProgressSpinnerModule,
-  ],
-  providers: [
-    RequestLoadingService,
-    { provide: HTTP_INTERCEPTORS, useClass: TimeoutHttpInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: LoadingHttpInterceptor, multi: true },
-    { provide: DEFAULT_TIMEOUT, useValue: 30000 },
-    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' } },
-  ],
+  declarations: [AppComponent, LogOnComponent],
+  imports: [AppRoutingModule, TcpNgUiModule],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
